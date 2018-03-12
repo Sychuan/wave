da91939becfb97c9b7185c4f0fbbc898fb29f0dc
Sychuan <macumoto@gmail.com>
2018-03-13 00:35:34 +0200

    Add files via upload

--- wave.py
+++ wave.py
@@ -18,12 +18,9 @@ from timeit import default_timer as timer
 
 
 class ColorM():
-    startR = 0
-    startG = 0    
-    startB = 0
-    endR = 255
-    endG = 149
-    endB = 0
+    startRGB = (0,0,0)
+    endRGB = (255,149,0)   
+    hue = 0.5
     colorscale = 0
     counter = 0
     
@@ -31,83 +28,95 @@ class ColorM():
          return (1-t)*x+t*y
 
 class interferention:
+    window = pygame.display.get_surface()
     title = "untitled"
-    rangexy = 100
-    typeM = True
-    expR = 0
-    count = 10
-    pos = [0,0]
-    C = 0#-0.621#-1+0.1j
-    z0 = 0
-    Cm = 1.3+2j
-    trap = 1
-    fracnum = 1
+    rangexy = (200,200)    
+    pos = [0,0]    
     method = 0
     date = 0
-    
-    scale = 5
+    counter = 0
+    scale = 10
+    directory = ""
  
   
 
 #-----main render function------------------------------------------------------------------------
+
 class wave:
-    A = 1 #A/r amplitude
-    w = 0.5 #krugovaya chastota
-    k = 12 #wave number
+    A = 3 #A/r amplitude
+    w = 2 #krugovaya chastota
+    k = 1 #wave number
     t = 0
     
+def coloring2(z):
+        if z.real<0:            
+            ColorM.hue=0.2
+        else:
+            ColorM.hue=0.0        
+        rgb_color = colorsys.hls_to_rgb(ColorM.hue, (abs(z.real/4))%1,1)                   
+        try:
+            
+            w=(int(rgb_color[0]*255),int(rgb_color[1]*255),int(rgb_color[2]*255))
+        except:
+            w = (0,0,0)
+        return w
+        
+    
+def coloring(z):       
+        try:
+            w=(ColorM.lerp(0,255,(abs(z.real/4))%1),ColorM.lerp(0,149,(abs(z.real/4))%1),ColorM.lerp(0,0,(abs(z.real/4))%1))      
+            #w =(ColorM.lerp(0,255,(abs(z/4))%1),ColorM.lerp(0,149,(abs(z/4))%1),ColorM.lerp(0,0,(abs(z/4))%1))  
+        except:
+            w = (0,0,0)
+        return w
+        
+     
+def spherical_wave(z):        
+        #z = (wave.A * np.cos(wave.w*wave.t-wave.k*abs(z+1)))
+        z1, z2 = wave.A/np.abs(z+5)*np.exp(1j*wave.w*wave.t-1j*wave.k*np.abs(z+5)), wave.A/np.abs(z-5)*np.exp(1j*wave.w*wave.t-1j*wave.k*abs(z-5))
+        #z = np.exp(1j*wave.k*np.abs(z))*np.exp(-1j*2*np.pi*wave.t)/np.abs(z)       
+        return  z1+z2
+   
+def start():
+        start = timer()
+        interferention.directory ="wave"+str(interferention.date)    
+        try:
+            os.makedirs(interferention.directory)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise    
+        interferention.date = datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
+        pygame.init()  
+        pygame.display.set_mode((interferention.rangexy[0],interferention.rangexy[1]))
+        interferention.window = pygame.display.get_surface()
+        x = np.arange(-(interferention.rangexy[0]/2),(interferention.rangexy[0]/2))
+        y = np.arange(-(interferention.rangexy[1]/2),(interferention.rangexy[1]/2))
+        xx, yy = np.meshgrid(x, y, sparse=True)
+        z = (xx+1j*yy)/interferention.scale-(interferention.pos[0]/interferention.scale-1j*interferention.pos[1]/interferention.scale)
+        
+        npcolor = np.frompyfunc(coloring,1,1)
+        end = timer()
+        print("finished time:"+str(end - start))
+        return z, npcolor
   
-def spherical_wave(z):
-    
-    #z = (wave.A * np.cos(wave.w*wave.t-wave.k*abs(z+1)))
-    z = wave.A/np.abs(z)*np.exp(1j*wave.w*wave.t-1j*wave.k*abs(z))
-    #z = np.exp(1j*wave.k*np.abs(z))*np.exp(-1j*2*np.pi*wave.t)/np.abs(z)
-    
-    modul = abs(z)
-    phase = np.angle(z, deg=True)
-    if phase<0:
-        phase+=360
-    
-    rgb_color = colorsys.hls_to_rgb(1,phase/360,1) 
-    try:
-        w=(int(rgb_color[0]*255),int(rgb_color[1]*255),int(rgb_color[2]*255))
-    except:
-        w = (0,0,0)
-    return w
-
-def render(dz,counter,new):   
-    start = timer()
-    pygame.init()
-    pygame.display.set_mode((interferention.rangexy,interferention.rangexy))
-    window = pygame.display.get_surface()
-    
-    directory ="wave"+str(interferention.date)
-    
-    try:
-        os.makedirs(directory)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
-    x = np.arange(-(interferention.rangexy/2),(interferention.rangexy/2))
-    y = np.arange(-(interferention.rangexy/2),(interferention.rangexy/2))
-    xx, yy = np.meshgrid(x, y, sparse=True)
-    z = (xx+1j*yy)/interferention.scale-(interferention.pos[0]/interferention.scale-1j*interferention.pos[1]/interferention.scale)+dz
-    npwave = np.frompyfunc(spherical_wave,1,1)
-    z = npwave(z)
-    w = np.array([list(arr) for arr in z])
-    w = np.flipud(np.fliplr(np.rot90(w))) 
-    w = ndimage.gaussian_filter(w, sigma=0.5)
-        #pxarray = pygame.pixelarray
-    pygame.surfarray.blit_array(window,w)
-    pygame.display.flip()
-    #showing final image          
-    pxarray = None       
-    #saving image
-    pygame.image.save(window,interferention.title+"_xy"+str(interferention.rangexy)+".png")
-    end = timer()
-    print("finished time:"+str(end - start))
-    
-    print('-'*15+'end'+'-'*15)    
+def render(z,counter,new,npwave):   
+        
+         
+        z = npwave(spherical_wave(z))
+        z = np.array([list(arr) for arr in z])
+        z = np.flipud(np.fliplr(np.rot90(z))) 
+        #w = ndimage.gaussian_filter(w, sigma=0.5)
+            #pxarray = pygame.pixelarray
+        pygame.surfarray.blit_array(interferention.window,z)
+        pygame.display.flip()
+        #showing final image          
+        
+        
+        interferention.counter+=1
+        pygame.image.save(interferention.window,interferention.directory+"/"+interferention.title+"_xy"+str(interferention.rangexy[0])+str(interferention.rangexy[1])+str(interferention.counter)+".png")
+       
+        
+   
          
 
 
@@ -116,21 +125,22 @@ def render(dz,counter,new):
     
 #------------------------------------------------------------------------------
 
-    print('-'*15+'start'+'-'*15)
-    interferention.date = datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
+    
+   
     #initializing Pygame
     
 #render(0,0,True)
-    
-while True:
-    wave.t += 0.1
-    render(0,0,True)    
+
+z, npwave = start()    
+while True:        
     for event in pygame.event.get():
-     if event.type == pygame.QUIT:            
-            
+        if event.type == pygame.QUIT:      
             pygame.quit()
-    
-    
+    start = timer()
+    wave.t += 0.1
+    render(z,0,True,npwave)
+    end = timer()
+    print("finished time:"+str(end - start))
 
 
  
